cd66510999534e4eccca356b9c7511a4843487f9
Jan Biernacik <jan@biernacik.dev>
2025-11-06 10:31:01 +0100

    feat: add iframe for OpenStreetMap in the main page; update Navbar styles for better UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,8 @@
 </script>
 
 <Topbar />
-<p>Noch nichts zu mieten</p>
+<iframe
+	class="h-screen w-full"
+	title="Karte"
+	src="https://www.openstreetmap.org/export/embed.html?bbox=-7.27294921875%2C44.63739123445587%2C28.1689453125%2C57.16007826737998&amp;layer=mapnik"
+></iframe>
